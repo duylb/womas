ea7ff8026b75af8411d217279a853010a0522f53
duylb <lebaoduy@gmail.com>
2026-02-25 14:58:09 +0700

    Refactor: Simplify navbar rendering by removing page buttons and using a static message

--- components/navbar.py
+++ components/navbar.py
@@ -1,20 +1,4 @@
 import streamlit as st
 
-PAGES = ["Dashboard", "Roster", "Payroll", "Staff", "Shifts"]
-
 def render_navbar():
-
-    col1, col2 = st.columns([2, 8])
-
-    with col1:
-        st.markdown("## 🗓 RosMan")
-
-    with col2:
-        nav_cols = st.columns(len(PAGES))
-
-        for i, page in enumerate(PAGES):
-            if nav_cols[i].button(page, key=f"nav_{page}", use_container_width=True):
-                st.session_state["page"] = page
-                st.rerun()
-
-    st.markdown("---")
+    st.write("🚀 NAVBAR IS HERE")
